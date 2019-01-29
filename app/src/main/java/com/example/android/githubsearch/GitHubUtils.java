1aa9d59c11c70c3b01e55a84ebb047d0f7746814
Rob Hess <robwhess@gmail.com>
2019-01-29 13:55:54 -0800

    Add GitHubUtils class.

---
+++ app/src/main/java/com/example/android/githubsearch/GitHubUtils.java
@@ -0,0 +1,8 @@
+package com.example.android.githubsearch;
+
+public class GitHubUtils {
+    final static String GITHUB_SEARCH_BASE_URL = "https://api.github.com/search/repositories";
+    final static String GITHUB_SEARCH_QUERY_PARAM = "q";
+    final static String GITHUB_SEARCH_SORT_PARAM = "sort";
+    final static String GITHUB_SEARCH_SORT_VALUE = "stars";
+}
